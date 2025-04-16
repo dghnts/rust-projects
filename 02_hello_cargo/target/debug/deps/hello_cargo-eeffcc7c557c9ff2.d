27dadbef983e4245ce19ed9b5cbd622eb607cf34
dghnts <dghnts6714@aol.com>
2025-04-16 22:35:13 +0900

    Initial commit: Add Rust Book chapter projects

---
+++ 02_hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2.d
@@ -0,0 +1,5 @@
+/home/dghnts/github/rust-projects/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2: src/main.rs
+
+/home/dghnts/github/rust-projects/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2.d: src/main.rs
+
+src/main.rs:
---
+++ 02_hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/dghnts/github/rust-projects/hello_cargo/target/debug/hello_cargo: /home/dghnts/github/rust-projects/hello_cargo/src/main.rs
