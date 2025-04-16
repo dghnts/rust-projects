c2db6889068f19f71581b9fa136d2f28735d04f5
dghnts <dghnts6714@aol.com>
2025-04-16 22:42:46 +0900

    Remove target/ from tracking and update .gitignore

--- 02_hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2.d
+++
@@ -1,5 +0,0 @@
-/home/dghnts/github/rust-projects/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2: src/main.rs
-
-/home/dghnts/github/rust-projects/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2.d: src/main.rs
-
-src/main.rs:
--- 02_hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/dghnts/github/rust-projects/hello_cargo/target/debug/hello_cargo: /home/dghnts/github/rust-projects/hello_cargo/src/main.rs
